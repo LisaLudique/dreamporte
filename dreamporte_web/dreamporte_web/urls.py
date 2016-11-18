f878862877430e6d2fd1bf48905d90dee06f6c0c
RGologorsky <rgologorsky@college.harvard.edu>
2016-11-18 10:19:36 -0500

    got static files working and css titlecard

--- dreamporte_web/dreamporte_web/urls.py
+++ dreamporte_web/dreamporte_web/urls.py
@@ -21,13 +21,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns
-
+from dreamporte_web.views import homepage
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
 urlpatterns += [
     url(r'^notifications/', get_nyt_pattern()),
+    url(r'^homepage/$', homepage),
     url(r'', get_wiki_pattern()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
